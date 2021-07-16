effad040a670a28786424e1b1674faf80f91a663
Ricardo Salazar <no-replay>
2021-07-15 20:45:42 -0500

    add send file function

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public' )));
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/notes", function(req, res) {
+res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
