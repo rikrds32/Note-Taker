4600367128e95bc7424cc97f7307c821dea7a282
Ricardo Salazar <no-replay>
2021-07-15 20:31:16 -0500

    add express for parsing

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3030;
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public' )));
 
 
